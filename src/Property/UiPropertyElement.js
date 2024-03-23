00c1e52cabc07234cad0c4cc3320c02d5ea7eaa6
Keith Clark <keith@keithclark.co.uk>
2024-03-23 07:17:07 +0000

    Uset iconlabel in property component

--- src/Property/UiPropertyElement.js
+++ src/Property/UiPropertyElement.js
@@ -4,19 +4,24 @@ import stylesheet from './styles.css';
 
 
 
-export default class extends UiElement {
+export default class UiPropertyElement extends UiElement {
 
   #labelElem;
 
   constructor() {
     super(template, stylesheet);
-    this.#labelElem = this.shadowRoot.querySelector('span');
+    this.#labelElem = this.shadowRoot.querySelector('ui-iconlabel');
   }
 
   static get observedAttributes() {
-    return ['label'];
+    return ['label', 'icon'];
   }
 
+  /**
+   * Sets or retrieves the text displayed in the component. If the value is an 
+   * empty string, no text will be displayed.
+   * @type {string} 
+   */
   get label() {
     return this.#labelElem.textContent;
   }
@@ -25,11 +30,24 @@ export default class extends UiElement {
     this.#labelElem.textContent = value;
   }
 
+  /**
+   * Sets or retrieves the icon displayed in the component. If the value is an 
+   * empty string, no icon will be displayed.
+   * @type {string} 
+   */
+  get icon() {
+    return this.#labelElem.icon;
+  }
 
+  set icon(value) {
+    this.#labelElem.setAttribute('icon', value);
+  }
 
   attributeChangedCallback(name, oldValue, newValue) {
     if (name === 'label') {
       this.label = newValue;
+    } else if (name === 'icon') {
+      this.icon = newValue;
     }
   }
 
