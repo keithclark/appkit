3bf1fe6d1dfb61a28383feb0e373b2539474b48a
Keith Clark <keith@keithclark.co.uk>
2024-03-23 07:20:06 +0000

    Add Toggle Button component

---
+++ src/ToggleButton/UiToggleButtonElement.js
@@ -0,0 +1,45 @@
+import UiButtonElement from '../Button/UiButtonElement.js';
+import stylesheet from './styles.css';
+
+/**
+ *
+ */
+export default class UiToggleButtonElement extends UiButtonElement {
+
+  #clickHandler;
+
+  constructor() {
+    super();
+    this.shadowRoot.adoptedStyleSheets.push(stylesheet);
+    this.#clickHandler = this.#handleClickEvent.bind(this);
+  }
+
+
+  /**
+   * Sets or retrieves the pressed state in the button. If the value is an 
+   * empty string, no icon will be displayed.
+   * @type {boolean} 
+   */
+  get pressed() {
+    return this.hasAttribute('pressed');
+  }
+
+  set pressed(value) {
+    this.toggleAttribute('pressed', value);
+  }
+
+  connectedCallback() {
+    this.addEventListener('click', this.#clickHandler);
+  }
+
+  /**
+   * 
+   * @param {MouseEvent} event 
+   */
+  #handleClickEvent(event) {
+    if (event.defaultPrevented) {
+      return;
+    }
+    this.pressed = !this.pressed;
+  }
+}
